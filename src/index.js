3ebf23309e1800e82d2a42ed134bdf3af1b3004c
Roman Sudakov <goa-slon@mail.ru>
2022-07-02 11:23:21 +0300

    init: start HumanReadableNumber

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+
+   let numeric = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+   let twoDigit = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+   let num = String(number).split('');
+   let humanReadableNumber;   
+
+   if (number == 0) { return ('zero'); }
+
+   if (num.length <= 2 && number < 20) {
+      humanReadableNumber = (numeric[number]);
+   } else if (num.length == 2 && number >= 20) {
+      humanReadableNumber =  (twoDigit[num[0]] + ' ' + numeric[num[1]]);
+   } else if (num.length == 3 && +(num[1] + num[2]) < 20) {
+      humanReadableNumber = (numeric[num[0]] + ' ' + 'hundred' + ' ' + numeric[+(num[1] + num[2])]);
+   } else if (num.length == 3 && +(num[1] + num[2]) >= 20) {
+      humanReadableNumber = (numeric[num[0]] + ' ' + 'hundred' + ' ' + twoDigit[+num[1]] + ' ' + numeric[+num[2]]);
+   } 
+
+   return humanReadableNumber.trim();
+   
+} 
